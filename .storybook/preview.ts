097050383187ea06f2988d9e01ad607fc68dc80e
karlyitianli <karl.yitian.li@gmail.com>
2024-07-09 22:57:24 -0400

    initial commit

---
+++ .storybook/preview.ts
@@ -0,0 +1,36 @@
+import type { Preview, ReactRenderer } from "@storybook/react";
+import { withThemeFromJSXProvider } from '@storybook/addon-themes';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import lightTheme from '../src/themes/lightTheme';
+import darkTheme from '../src/themes/darkTheme';
+
+// Load Material UI fonts
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import '@fontsource/material-icons';
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+
+  decorators: [
+    withThemeFromJSXProvider<ReactRenderer>({
+      themes: {
+        light: lightTheme,
+        dark: darkTheme,
+      },
+      defaultTheme: 'light',
+      Provider: ThemeProvider,
+      GlobalStyles: CssBaseline,
+    })]
+};
+
+export default preview;
