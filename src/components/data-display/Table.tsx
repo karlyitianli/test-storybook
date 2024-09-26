10a9a9d53e3a109110977550c8a9ccfa329a1066
karlyitianli <karl.yitian.li@gmail.com>
2024-09-25 20:34:32 -0400

    fixed tooltip

--- src/components/data-display/Table.tsx
+++ src/components/data-display/Table.tsx
@@ -1,8 +1,43 @@
 import React from 'react';
-import Table, { TableProps as MUITableProps } from '@mui/material/Table';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
-export interface TableProps extends MUITableProps {}
+export default {
+  title: 'Data Display/Table',
+  component: Table,
+} as ComponentMeta<typeof Table>;
 
-export const MUITable: React.FC<TableProps> = ({ children, ...props }) => {
-  return <Table {...props}>{children}</Table>;
-};
+const Template: ComponentStory<typeof Table> = (args) => (
+  <TableContainer component={Paper}>
+    <Table {...args}>
+      <TableHead>
+        <TableRow>
+          <TableCell>Header 1</TableCell>
+          <TableCell>Header 2</TableCell>
+          <TableCell>Header 3</TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        <TableRow>
+          <TableCell>Row 1, Cell 1</TableCell>
+          <TableCell>Row 1, Cell 2</TableCell>
+          <TableCell>Row 1, Cell 3</TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell>Row 2, Cell 1</TableCell>
+          <TableCell>Row 2, Cell 2</TableCell>
+          <TableCell>Row 2, Cell 3</TableCell>
+        </TableRow>
+      </TableBody>
+    </Table>
+  </TableContainer>
+);
+
+export const Default = Template.bind({});
+Default.args = {};
--- src/stories/data-display/Tooltip.stories.tsx
+++ src/stories/data-display/Tooltip.stories.tsx
@@ -1,20 +1,49 @@
 import React from 'react';
 import { Meta, Story } from '@storybook/react';
-import Tooltip from 'components/data-display/Tooltip';
+import { MUITooltip } from '../../components/data-display/Tooltip';
+import Button from '@mui/material/Button';
 
 export default {
   title: 'Data Display/Tooltip',
-  component: Tooltip,
+  component: MUITooltip,
+  argTypes: {
+    placement: {
+      control: {
+        type: 'select',
+        options: ['bottom-end', 'bottom-start', 'bottom', 'left-end', 'left-start', 'left', 'right-end', 'right-start', 'right', 'top-end', 'top-start', 'top'],
+      },
+    },
+    arrow: {
+      control: {
+        type: 'boolean',
+      },
+    },
+  },
 } as Meta;
 
-const Template: Story = (args) => <Tooltip {...args} />;
+const Template: Story = (args) => (
+  <MUITooltip {...args}>
+    <Button variant='contained'>Hover over me</Button>
+  </MUITooltip>
+);
 
 export const Default = Template.bind({});
 Default.args = {
-  // Add default props here
+  title: 'Default Tooltip',
+  placement: 'bottom',
+  arrow: false,
 };
 
-export const WithContent = Template.bind({});
-WithContent.args = {
-  // Add props to display tooltip with content
+export const WithCustomPlacement = Template.bind({});
+WithCustomPlacement.args = {
+  title: 'Tooltip with custom placement',
+  placement: 'right',
+  arrow: false,
+};
+
+export const WithArrow = Template.bind({});
+WithArrow.args = {
+  title: 'Tooltip with arrow',
+  placement: 'bottom',
+  arrow: true,
 };
